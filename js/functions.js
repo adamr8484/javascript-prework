85a08de2a1d230be8c66e1649ddbc87616d06fea
Adam Rajba <adamrajba@gmail.com>
2020-10-02 18:59:36 +0100

    first commit

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,15 @@
 function printMessage(msg){
 	let div = document.createElement('div');
 	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+    document.getElementById('messages').appendChild(div);
 }
 
 function clearMessages(){
 	document.getElementById('messages').innerHTML = '';
+}
+
+function printMessage {
+    let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 }
