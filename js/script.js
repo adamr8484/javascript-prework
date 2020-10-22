5e2ae895f954e558f9aba46dc904d105157ce423
Adam Rajba <adamrajba@gmail.com>
2020-10-22 20:06:10 +0100

    correct playgame

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ document.getElementById('play-rock').addEventListener('click',function(){
     playGame(1);
 });
 document.getElementById('play-paper').addEventListener('click',function(){
-    playGame(3);
+    playGame(2);
 });
 document.getElementById('play-scissors').addEventListener('click',function(){
     playGame(3);
